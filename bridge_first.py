4fc4c07a2491a65a2c415549f1e25ea47ba2cca8
adl5423 <aisenlopezjr@gmail.com>
2024-07-29 13:38:23 -0400

    Automated bug fixes

--- bridge_first.py
+++ bridge_first.py
@@ -1,43 +1,43 @@
+```python
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-*References:
-http://en.wikibooks.org/wiki/Computer_Science_Design_Patterns/Bridge_Pattern#Python
+import doctest
 
-*TL;DR
-Decouples an abstraction from its implementation.
-"""
-
-
-# ConcreteImplementor 1/2
-class DrawingAPI1(object):
+class DrawingAPI1:
     def draw_circle(self, x, y, radius):
-        print('API1.circle at {}:{} radius {}'.format(x, y, radius))
+        """Draw a circle with given coordinates and radius."""
+        print(f'API1.circle at {x}:{y} radius {radius}')
+    
+    def another_method(self):
+        pass
 
+    def another_public_method(self):
+        pass
 
-# ConcreteImplementor 2/2
-class DrawingAPI2(object):
+class DrawingAPI2:
     def draw_circle(self, x, y, radius):
-        print('API2.circle at {}:{} radius {}'.format(x, y, radius))
+        """Draw a circle with given coordinates and radius."""
+        print(f'API2.circle at {x}:{y} radius {radius}')
+    
+    def additional_method(self):
+        pass
 
-
-# Refined Abstraction
-class CircleShape(object):
+class CircleShape:
+    """Represents a circle shape with various geometric properties."""
     def __init__(self, x, y, radius, drawing_api):
         self._x = x
         self._y = y
         self._radius = radius
         self._drawing_api = drawing_api
 
-    # low-level i.e. Implementation specific
     def draw(self):
+        """Draw the bridge structure."""
         self._drawing_api.draw_circle(self._x, self._y, self._radius)
 
-    # high-level i.e. Abstraction specific
     def scale(self, pct):
+        """Scale the value by a given percentage."""
         self._radius *= pct
-
 
 def main():
     shapes = (CircleShape(1, 2, 3, DrawingAPI1()), CircleShape(5, 7, 11, DrawingAPI2()))
@@ -46,10 +46,8 @@ def main():
         shape.scale(2.5)
         shape.draw()
 
+    doctest.testmod()
 
 if __name__ == '__main__':
     main()
-
-### OUTPUT ###
-# API1.circle at 1:2 radius 7.5
-# API2.circle at 5:7 radius 27.5
+```
--- bridge_latest.py
+++ bridge_latest.py
@@ -1,38 +1,37 @@
-"""
-*References:
-http://en.wikibooks.org/wiki/Computer_Science_Design_Patterns/Bridge_Pattern#Python
-
-*TL;DR
-Decouples an abstraction from its implementation.
-"""
-
-
-# ConcreteImplementor 1/2
+```python
 class DrawingAPI1:
     def draw_circle(self, x, y, radius):
-        print(f"API1.circle at {x}:{y} radius {radius}")
+        print(f'API1.circle at {x}:{y} radius {radius}')
+    
+    def another_method(self):
+        pass
+
+    def another_public_method(self):
+        pass
 
 
-# ConcreteImplementor 2/2
 class DrawingAPI2:
     def draw_circle(self, x, y, radius):
-        print(f"API2.circle at {x}:{y} radius {radius}")
+        print(f'API2.circle at {x}:{y} radius {radius}')
+
+    def additional_method(self):
+        pass
 
 
-# Refined Abstraction
 class CircleShape:
+    """Represents a circle shape with various geometric properties."""
     def __init__(self, x, y, radius, drawing_api):
         self._x = x
         self._y = y
         self._radius = radius
         self._drawing_api = drawing_api
 
-    # low-level i.e. Implementation specific
     def draw(self):
+        """Draw the bridge structure."""
         self._drawing_api.draw_circle(self._x, self._y, self._radius)
 
-    # high-level i.e. Abstraction specific
     def scale(self, pct):
+        """Scale the value by a given percentage."""
         self._radius *= pct
 
 
@@ -50,5 +49,5 @@ def main():
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
+```
