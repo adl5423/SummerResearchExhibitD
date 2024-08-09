f40aeabae61721faf5866aa4246220bf11d95586
adl5423 <aisenlopezjr@gmail.com>
2024-08-08 22:41:06 -0400

    Automated bug fixes

--- pyanitools.py
+++ pyanitools.py
@@ -1,34 +1,31 @@
-# Written by Roman Zubatyuk and Justin S. Smith
+```python
+"""
+This module provides tools for working with ANI (Atomic Neural Networks) data.
+"""
+
+import os
 import h5py
 import numpy as np
 import platform
-import os
+import sys
 
 PY_VERSION = int(platform.python_version().split('.')[0]) > 3
 
 
 class datapacker(object):
+    """A class to handle data packing operations."""
 
     def __init__(self, store_file, mode='w-', complib='gzip', complevel=6):
-        """Wrapper to store arrays within HFD5 file
-        """
-        # opening file
         self.store = h5py.File(store_file, mode=mode)
         self.clib = complib
         self.clev = complevel
 
     def store_data(self, store_loc, **kwargs):
-        """Put arrays to store
-        """
-        #print(store_loc)
         g = self.store.create_group(store_loc)
-        for k, v, in kwargs.items():
-            #print(type(v[0]))
-
-            #print(k)
-            if type(v) == list:
+        for k, v in kwargs.items():
+            if isinstance(v, list):
                 if len(v) != 0:
-                    if type(v[0]) is np.str_ or type(v[0]) is str:
+                    if isinstance(v[0], (np.str_, str)):
                         v = [a.encode('utf8') for a in v]
 
             g.create_dataset(k,
@@ -37,83 +34,52 @@ class datapacker(object):
                              compression_opts=self.clev)
 
     def cleanup(self):
-        """Wrapper to close HDF5 file
-        """
         self.store.close()
 
 
 class anidataloader(object):
-    ''' Contructor '''
 
     def __init__(self, store_file):
         if not os.path.exists(store_file):
-            exit('Error: file not found - ' + store_file)
+            sys.exit(f'Error: file not found - {store_file}')
         self.store = h5py.File(store_file)
-
-    ''' Group recursive iterator (iterate through all groups in all branches and return datasets in dicts) '''
 
     def h5py_dataset_iterator(self, g, prefix=''):
         for key in g.keys():
             item = g[key]
-            path = '{}/{}'.format(prefix, key)
-            keys = [i for i in item.keys()]
-            if isinstance(item[keys[0]], h5py.Dataset):  # test for dataset
+            path = f'{prefix}/{key}'
+            if isinstance(item, h5py.Dataset):
                 data = {'path': path}
-                for k in keys:
-                    if not isinstance(item[k], h5py.Group):
-                        dataset = np.array(item[k])
-
-                        if type(dataset) is np.ndarray:
-                            if dataset.size != 0:
-                                if type(dataset[0]) is np.bytes_:
-                                    dataset = [
-                                        a.decode('ascii') for a in dataset
-                                    ]
-
-                        data.update({k: dataset})
-
+                dataset = np.array(item)
+                if isinstance(dataset, np.ndarray):
+                    if dataset.size != 0:
+                        if isinstance(dataset[0], np.bytes_):
+                            dataset = [a.decode('ascii') for a in dataset]
+                data.update({key: dataset})
                 yield data
-            else:  # test for group (go down)
+            else:
                 yield from self.h5py_dataset_iterator(item, path)
 
-    ''' Default class iterator (iterate through all data) '''
-
     def __iter__(self):
-        for data in self.h5py_dataset_iterator(self.store):
-            yield data
-
-    ''' Returns a list of all groups in the file '''
+        yield from self.h5py_dataset_iterator(self.store)
 
     def get_group_list(self):
-        return [g for g in self.store.values()]
-
-    ''' Allows interation through the data in a given group '''
+        return list(self.store.values())
 
     def iter_group(self, g):
-        for data in self.h5py_dataset_iterator(g):
-            yield data
-
-    ''' Returns the requested dataset '''
+        yield from self.h5py_dataset_iterator(g)
 
     def get_data(self, path, prefix=''):
         item = self.store[path]
-        path = '{}/{}'.format(prefix, path)
-        keys = [i for i in item.keys()]
+        path = f'{prefix}/{path}'
         data = {'path': path}
-        # print(path)
-        for k in keys:
-            if not isinstance(item[k], h5py.Group):
-                dataset = np.array(item[k].value)
-
-                if type(dataset) is np.ndarray:
-                    if dataset.size != 0:
-                        if type(dataset[0]) is np.bytes_:
-                            dataset = [a.decode('ascii') for a in dataset]
-
-                data.update({k: dataset})
+        dataset = np.array(item)
+        if isinstance(dataset, np.ndarray):
+            if dataset.size != 0:
+                if isinstance(dataset[0], np.bytes_):
+                    dataset = [a.decode('ascii') for a in dataset]
+        data.update({path.split('/')[-1]: dataset})
         return data
-
-    ''' Returns the number of groups '''
 
     def group_size(self):
         return len(self.get_group_list())
@@ -124,7 +90,7 @@ class anidataloader(object):
             count = count + len(g.items())
         return count
 
-    ''' Close the HDF5 file '''
-
     def cleanup(self):
+        """Clean up resources and perform necessary cleanup actions."""
         self.store.close()
+```
